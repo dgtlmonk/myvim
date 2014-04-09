2dbad84ec10e48257221e6b41ef8bc0ded51eb61
Joel Pablo <joel.pablo@digitalmonkstudio.com>
2014-04-10 00:24:38 +0800

    added tmux and obsidian configuration

---
+++ UltiSnips-2.2/UltiSnips/test.js
@@ -0,0 +1,13 @@
+:q
+q:= Ember.View.extend({
+
+    tagName: , // <string>
+    templateName: null,
+    template: null, // <string> .hbs id
+    defaultTemplate: Ember.Handlebars.compile('I was the default'),
+    classNames:[], // <string array>
+
+
+});
+
+
